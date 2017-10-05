4a78938c9e948346f4bfe8fd82a1dabdec331b25
Ashnita Bali <ashnita01@gmail.com>
2017-10-05 22:52:21 +0100

    pass data to child component

--- app/app.module.ts
+++ app/app.module.ts
@@ -3,12 +3,14 @@ import { BrowserModule } from '@angular/platform-browser'
 
 import { EventsAppComponent } from './events.app.component';
 import { EventListComponent } from './events/events-list.component';
+import { EventThumbNailComponent } from './events/event-thumbnail.component';
 
 @NgModule({
     imports: [ BrowserModule ],
     declarations: [ 
         EventsAppComponent,
-        EventListComponent
+        EventListComponent,
+        EventThumbNailComponent
     ],
     bootstrap: [ EventsAppComponent ]
 })
