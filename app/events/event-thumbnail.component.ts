94a8652a275f760ef9e788846ca460f050555dc9
Ashnita Bali <ashnita01@gmail.com>
2017-10-06 13:56:53 +0100

    use *ngSwitch

--- app/events/event-thumbnail.component.ts
+++ app/events/event-thumbnail.component.ts
@@ -6,7 +6,12 @@ import { Component, Input } from '@angular/core';
         <div class="well hoverwell thumbnail">
             <h2>{{event.name}}</h2>
             <div>Date: {{event.date}}</div>
-            <div>Time: {{event.time}}</div>
+            <div [ngSwitch]="event?.time">
+                Time: {{event.time}}
+                <span *ngSwitchCase="'8:00 am'">(Early Start)</span>
+                <span *ngSwitchCase="'10:00 am'">(Late Start)</span>
+                <span *ngSwitchDefault>(Normal Start)</span>
+            </div>
             <div>Price: \${{event.price}}</div>
             <div>
                 <span>Location: {{event.location.address}}</span>
@@ -15,6 +20,7 @@ import { Component, Input } from '@angular/core';
         </div>
     `,
     styles: [`
+        .thumbnail { min-height: 210px; }
         .pad-left { margin-left: 10px; }
         .well div { color: #bbb }
     `]
