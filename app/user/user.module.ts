9e0a0471dc871af2dac9734ad342805f216b058b
Ashnita Bali <ashnita01@gmail.com>
2017-10-09 23:33:49 +0100

    create user profile as a reactive form

--- app/user/user.module.ts
+++ app/user/user.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { ProfileComponent } from './profile.component';
 import { LoginComponent } from './login.component';
@@ -10,6 +10,7 @@ import userRoutes  from './user.routes';
     imports: [
         CommonModule,
         FormsModule,
+        ReactiveFormsModule,
         userRoutes
     ],
     declarations: [
