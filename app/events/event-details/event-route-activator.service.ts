adf48f14db93f3217c9baddf346d3e12a6397a08
Ashnita B <ashnita01@gmail.com>
2017-10-14 23:23:36 +0100

    Remove console.log

--- app/events/event-details/event-route-activator.service.ts
+++ app/events/event-details/event-route-activator.service.ts
@@ -11,8 +11,6 @@ export class EventRouteActivator implements CanActivate {
 
     canActivate(route:ActivatedRouteSnapshot) {
         const eventExists = !!this.eventService.getEvent(+route.params['id']);
-
-        console.log("eventExists", eventExists);
         
         if (!eventExists) {
             this.router.navigate(['/404']);
